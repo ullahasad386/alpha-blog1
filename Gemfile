0527848068dd1e408ffc4b1272d14a112ad7daae
Asad Ullah <ullahasad386@gmail.com>
2019-05-26 18:08:23 +0500

    create users table and model

--- Gemfile
+++ Gemfile
@@ -46,8 +46,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
 group :production do
-  gem 'pg'
+  gem 'pg', '~> 0.11'
   gem 'rails_12factor'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190128025404) do
+ActiveRecord::Schema.define(version: 20190526130246) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "desciption"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
