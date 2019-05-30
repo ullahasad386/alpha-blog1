2484d65d089506de30ab5f2cd8db0b952cfc4859
Asad Ullah <ullahasad386@gmail.com>
2019-05-30 10:07:05 +0500

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:edit, :show, :update, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -49,6 +51,13 @@ class ArticlesController < ApplicationController
   end
   def article_params
     params.require(:article).permit(:title, :desciption)
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can only edit and delete your own articles."
+      redirect_to root_path
+    end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user,  only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -14,12 +17,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
-  end
+   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
@@ -29,7 +29,6 @@ class UsersController < ApplicationController
   end
 
 def show
-  @user = User.find(params[:id])
   @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 end
 
@@ -42,5 +41,15 @@ end
     params.require(:user).permit(:username, :email, :password)
   end
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if !logged_in? || current_user != @user
+      flash[:danger] = "You can only edit your own account."
+      redirect_to root_path
+    end
+  end
 
 end
