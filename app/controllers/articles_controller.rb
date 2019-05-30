0cc3159d02aec50eccc76468c382372cd634da59
Asad Ullah <ullahasad386@gmail.com>
2019-05-30 11:07:15 +0500

    Changes in create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @aritcle.user = current_user
+    @article.user = current_user
     if @article.save
       flash[:success] = "The article was successfully created."
       redirect_to article_path(@article)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-
       flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
       redirect_to user_path(@user)
     else
