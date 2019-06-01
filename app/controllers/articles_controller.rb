e7cbecd4f561ee7d6753429d7cc0f9c0bd29bc09
Asad Ullah <ullahasad386@gmail.com>
2019-06-01 23:20:09 +0500

    Update all views with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -50,7 +50,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   def article_params
-    params.require(:article).permit(:title, :desciption)
+    params.require(:article).permit(:title, :desciption, category_ids: [])
   end
 
   def require_same_user
