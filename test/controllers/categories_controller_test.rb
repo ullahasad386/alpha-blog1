1b84f8be368e78e25c58d505c205fcb642e6fdbb
Asad Ullah <ullahasad386@gmail.com>
2019-06-01 17:57:07 +0500

    Add admin requirement to create categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,7 @@ class CategoriesControllerTest < ActionController::TestCase
 
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "Asad", email: "asad@example.com", password: "password", admin: true)
   end
 
   test "should get categories index" do
@@ -13,6 +14,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
@@ -20,6 +22,13 @@ class CategoriesControllerTest < ActionController::TestCase
   test "should get show" do
     get(:show, {'id' => @category.id})
     assert_response :success
+  end
+
+  test "should redirect create when admin is not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: { name: "sports"}
+    end
+    assert_redirected_to categories_path
   end
 
 
